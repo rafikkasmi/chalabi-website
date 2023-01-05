490163ff709aeb1c2f74b465a123f77a9fc5071b
rafikkasmi <rafikhdey@gmail.com>
2023-01-05 14:51:30 +0100

    deleting files

--- app/Http/Controllers/Admin/FrontendController.php
+++ app/Http/Controllers/Admin/FrontendController.php
@@ -9,7 +9,7 @@ class FrontendController extends Controller
 {
     public function index()
     {
-        return view('admin.index');
+        return redirect('/admin/products');
     }
     public function users()
     {
