1e7caf4dcb0274901d1cf507c98706ec7d6c6c0a
rafikkasmi <rafikhdey@gmail.com>
2023-01-05 21:30:47 +0100

    fixing title in login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Chalabi Store Admin Panel Login</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
